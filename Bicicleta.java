432b480099fddc1a42dfbd90381f755136916308
Máximo Fernández Riera <43608040+maximofernandezriera@users.noreply.github.com>
2024-02-15 09:57:00 +0100

    Create Bicicleta.java

---
+++ Bicicleta.java
@@ -0,0 +1,12 @@
+public class Bicicleta extends Vehiculo {
+
+  private int pinones; // número de piñones
+  
+  public Bicicleta(int p) {
+    this.pinones = p;
+  }
+
+  public void hazCaballito() {
+    System.out.println("Estoy haciendo el caballito");
+  }
+}
